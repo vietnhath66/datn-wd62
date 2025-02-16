af932d1d5d07872666117bf5e285b9077ce994af
vietnhath66 <nhatnvph41576@fpt.edu.vn>
2025-02-17 00:56:33 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ Route::group(['prefix' => 'client', 'as' => 'client.'], function () {
     Route::get('home', [HomeController::class, 'viewHome'])->name('viewHome');
 });
 
-
+// Admin
 Route::prefix('admin')
     ->as('admin.')
     ->middleware(['admin', 'locale', 'backend_default_locale'])
@@ -24,7 +24,3 @@ Route::get('admin/login', [AuthController::class, 'index'])->name('auth.admin')-
 Route::post('login', [AuthController::class, 'login'])->name('auth.login');
 Route::get('admin/logout', [AuthController::class, 'logout'])->name('auth.logout');
 
-
-Route::get('/', function () {
-    return view('welcome');
-});
