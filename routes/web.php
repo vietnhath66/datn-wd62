c0e450a52233c80d393dbc1a551e3e54477f8d4b
vietnhath66 <157140777+vietnhath66@users.noreply.github.com>
2025-02-17 00:29:44 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,25 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Client\HomeController;
-
+use App\Http\Controllers\Backend\DashboardController;
+use App\Http\Controllers\Backend\AuthController;
 
 // Client
 Route::group(['prefix' => 'client', 'as' => 'client.'], function () {
 
     Route::get('home', [HomeController::class, 'viewClient'])->name('viewClient');
 });
-use App\Http\Controllers\Backend\DashboardController;
-use App\Http\Controllers\Backend\AuthController;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
 
 Route::prefix('admin')
     ->as('admin.')
