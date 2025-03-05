8a5c11c8899353e811a2b5ef8259e3509eaf2c3c
Nguyễn Tiến Dũng <nguyentiendung@MacBookk-Pro-14-ca-Dng.local>
2025-03-05 22:53:51 +0700

    Sửa contact

--- resources/views/client/contact/contentContact.blade.php
+++ resources/views/client/contact/contentContact.blade.php
@@ -1,64 +1,69 @@
 <section class="bg0 p-t-104 p-b-116">
-    <div class="container">
-        <div class="flex-w flex-tr">
-            <div class="size-210 bor10 p-lr-70 p-t-55 p-b-70 p-lr-15-lg w-full-md">
-                <form>
-                    <h4 class="mtext-105 cl2 txt-center p-b-30">
-                        Liên hệ
-                    </h4>
+		<div class="container">
+			<div class="flex-w flex-tr">
+				<div class="size-210 bor10 p-lr-70 p-t-55 p-b-70 p-lr-15-lg w-full-md">
+					<form>
+						<h4 class="mtext-105 cl2 txt-center p-b-30">
+							Liên hệ
+						</h4>
 
-                    <div class="bor8 m-b-20 how-pos4-parent">
-                        <input class="stext-111 cl2 plh3 size-116 p-l-62 p-r-30" type="text" name="email"
-                            placeholder="Email">
-                        <img class="how-pos4 pointer-none" src="images/icons/icon-email.png" alt="ICON">
-                    </div>
+						<div class="bor8 m-b-20 how-pos4-parent">
+							<input class="stext-111 cl2 plh3 size-116 p-l-62 p-r-30" type="text" name="email" placeholder="Email">
+							<img class="how-pos4 pointer-none" src="images/icons/icon-email.png" alt="ICON">
+						</div>
 
-                    <div class="bor8 m-b-30">
-                        <textarea class="stext-111 cl2 plh3 size-120 p-lr-28 p-tb-25" name="msg"
-                            placeholder="Chúng tôi có thể giúp gì cho bạn ?"></textarea>
-                    </div>
+						<div class="bor8 m-b-30">
+							<textarea class="stext-111 cl2 plh3 size-120 p-lr-28 p-tb-25" name="msg" placeholder="Chúng tôi có thể giúp gì cho bạn ?"></textarea>
+						</div>
 
-                    <button class="flex-c-m stext-101 cl0 size-121 bg3 bor1 hov-btn3 p-lr-15 trans-04 pointer">
-                        Gửi
-                    </button>
-                </form>
-            </div>
+						<button class="flex-c-m stext-101 cl0 size-121 bg3 bor1 hov-btn3 p-lr-15 trans-04 pointer">
+							Gửi 
+						</button>
+					</form>
+				</div>
 
-            <div class="size-210 bor10 flex-w flex-col-m p-lr-93 p-tb-30 p-lr-15-lg w-full-md">
+				<div class="size-210 bor10 flex-w flex-col-m p-lr-93 p-tb-30 p-lr-15-lg w-full-md">
+					
 
+					<div class="flex-w w-full p-b-42">
+						<span class="fs-18 cl5 txt-center size-211">
+						<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M6.62 10.79C8.06 13.62 10.38 15.94 13.21 17.38L15.41 15.18C15.69 14.9 16.08 14.82 16.43 14.93C17.55 15.3 18.75 15.5 20 15.5C20.55 15.5 21 15.95 21 16.5V20C21 20.55 20.55 21 20 21C10.61 21 3 13.39 3 4C3 3.45 3.45 3 4 3H7.5C8.05 3 8.5 3.45 8.5 4C8.5 5.25 8.7 6.45 9.07 7.57C9.18 7.92 9.1 8.31 8.82 8.59L6.62 10.79Z" fill="black"/>
+            </svg>
+						</span>
 
-                <div class="flex-w w-full p-b-42">
-                    <span class="fs-18 cl5 txt-center size-211">
-                        <span class="lnr lnr-phone-handset"></span>
-                    </span>
+						<div class="size-212 p-t-2">
+							<span class="mtext-110 cl2">
+								Số điện thoại hỗ trợ
+							</span>
 
-                    <div class="size-212 p-t-2">
-                        <span class="mtext-110 cl2">
-                            Số điện thoại hỗ trợ
-                        </span>
+							<p class="stext-115 cl1 size-213 p-t-18">
+								0968343848
+							</p>
+						</div>
+					</div>
 
-                        <p class="stext-115 cl1 size-213 p-t-18">
-                            0968343848
-                        </p>
-                    </div>
-                </div>
+					<div class="flex-w w-full">
+						<span class="fs-18 cl5 txt-center size-211">
+						<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M20 4H4C2.9 4 2 4.9 2 6V18C2 19.1 2.9 20 4 20H20C21.1 20 22 19.1 22 18V6C22 4.9 21.1 4 20 4ZM20 18H4V8L12 13L20 8V18ZM12 11L4 6H20L12 11Z" fill="black"/>
+            </svg>
+						</span>
 
-                <div class="flex-w w-full">
-                    <span class="fs-18 cl5 txt-center size-211">
-                        <span class="lnr lnr-envelope"></span>
-                    </span>
+						<div class="size-212 p-t-2">
+							<span class="mtext-110 cl2">
+								Email hỗ trợ
+							</span>
 
-                    <div class="size-212 p-t-2">
-                        <span class="mtext-110 cl2">
-                            Email hỗ trợ
-                        </span>
-
-                        <p class="stext-115 cl1 size-213 p-t-18">
-                            chicwear68@gmail.com
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
+							<p class="stext-115 cl1 size-213 p-t-18">
+								chicwear68@gmail.com
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>	
+	
+	
+	
