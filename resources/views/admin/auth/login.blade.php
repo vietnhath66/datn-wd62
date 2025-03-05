dc5a6406fa466acb5192bc1c1a2972251b507cbc
vietnhath66 <nhatnvph41576@fpt.edu.vn>
2025-03-05 11:05:12 +0700

    update

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -10,19 +10,19 @@
     <meta content="Minimal Admin & Dashboard Template" name="description">
     <meta content="StarCode Kh" name="author">
     <!-- App favicon -->
-    <link rel="shortcut icon" href="../theme/admin/html/assets/images/favicon.ico">
+    <link rel="shortcut icon" href="admin/images/favicon.ico">
     <!-- Layout config Js -->
-    <script src="../theme/admin/html/assets/js/layout.js"></script>
+    <script src="admin/js/layout.js"></script>
     <!-- Icons CSS -->
 
     <!-- StarCode CSS -->
 
-<!-- Latest compiled and minified CSS -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Latest compiled and minified CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
-<!-- Latest compiled JavaScript -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="../theme/admin/html/assets/css/starcode2.css">
+    <!-- Latest compiled JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="{{ asset('admin/css/starcode2.css') }}">
 </head>
 
 <body
@@ -31,10 +31,8 @@
     <div class="mb-0 border-none lg:w-[500px] card bg-white/70 shadow-none dark:bg-zink-500/70">
         <div class="!px-10 !py-12 card-body">
             <a href="index-1.html">
-                <img src="../theme/admin/html/assets/images/logo-light.png" alt=""
-                    class="hidden h-6 mx-auto dark:block">
-                <img src="../theme/admin/html/assets/images/logo-dark.png" alt=""
-                    class="block h-6 mx-auto dark:hidden">
+                <img src="images/logo-light.png" alt="" class="hidden h-6 mx-auto dark:block">
+                <img src="images/logo-dark.png" alt="" class="block h-6 mx-auto dark:hidden">
             </a>
 
             <div class="mt-8 text-center">
@@ -45,20 +43,26 @@
                 @csrf
                 <div class="mt-3">
                     <label for="email" class="inline-block mb-2 text-base font-medium">Email</label>
-                    <input type="text" class="form-input dark:bg-zink-600/50 border-slate-200 dark:border-zink-500 focus:outline-none focus:border-custom-500 disabled:bg-slate-100 dark:disabled:bg-zink-600 disabled:border-slate-300 dark:disabled:border-zink-500 dark:disabled:text-zink-200 disabled:text-slate-500 dark:text-zink-100 dark:focus:border-custom-800 placeholder:text-slate-400 dark:placeholder:text-zink-200" placeholder="Email" name="email" value="{{ request('email') ?: old('email') }}">
+                    <input type="text"
+                        class="form-input dark:bg-zink-600/50 border-slate-200 dark:border-zink-500 focus:outline-none focus:border-custom-500 disabled:bg-slate-100 dark:disabled:bg-zink-600 disabled:border-slate-300 dark:disabled:border-zink-500 dark:disabled:text-zink-200 disabled:text-slate-500 dark:text-zink-100 dark:focus:border-custom-800 placeholder:text-slate-400 dark:placeholder:text-zink-200"
+                        placeholder="Email" name="email" value="{{ request('email') ?: old('email') }}">
                     @error('email')
                         <span class="text-danger font-italic">* {{ $message }}</span>
                     @enderror
                 </div>
                 <div class="mt-3">
                     <label for="password" class="inline-block mb-2 text-base font-medium">Mật khẩu</label>
-                    <input type="password" class="form-input dark:bg-zink-600/50 border-slate-200 dark:border-zink-500 focus:outline-none focus:border-custom-500 disabled:bg-slate-100 dark:disabled:bg-zink-600 disabled:border-slate-300 dark:disabled:border-zink-500 dark:disabled:text-zink-200 disabled:text-slate-500 dark:text-zink-100 dark:focus:border-custom-800 placeholder:text-slate-400 dark:placeholder:text-zink-200" placeholder="Password" name="password">
+                    <input type="password"
+                        class="form-input dark:bg-zink-600/50 border-slate-200 dark:border-zink-500 focus:outline-none focus:border-custom-500 disabled:bg-slate-100 dark:disabled:bg-zink-600 disabled:border-slate-300 dark:disabled:border-zink-500 dark:disabled:text-zink-200 disabled:text-slate-500 dark:text-zink-100 dark:focus:border-custom-800 placeholder:text-slate-400 dark:placeholder:text-zink-200"
+                        placeholder="Password" name="password">
                     @error('password')
                         <span class="text-danger font-italic">* {{ $message }}</span>
                     @enderror
                 </div>
                 <div class="mt-10">
-                    <button type="submit" class="w-full text-white btn bg-custom-500 border-custom-500 hover:text-white hover:bg-custom-600 hover:border-custom-600 focus:text-white focus:bg-custom-600 focus:border-custom-600 focus:ring focus:ring-custom-100 active:text-white active:bg-custom-600 active:border-custom-600 active:ring active:ring-custom-100 dark:ring-custom-400/20">Đăng Nhập</button>
+                    <button type="submit"
+                        class="w-full text-white btn bg-custom-500 border-custom-500 hover:text-white hover:bg-custom-600 hover:border-custom-600 focus:text-white focus:bg-custom-600 focus:border-custom-600 focus:ring focus:ring-custom-100 active:text-white active:bg-custom-600 active:border-custom-600 active:ring active:ring-custom-100 dark:ring-custom-400/20">Đăng
+                        Nhập</button>
                 </div>
                 <div
                     class="relative text-center my-9 before:absolute before:top-3 before:left-0 before:right-0 before:border-t before:border-t-slate-200 dark:before:border-t-zink-500">
@@ -93,15 +97,14 @@
         </div>
     </div>
 
-    <script src='../theme/admin/html/assets/libs/choices.js/public/../theme/admin/html/assets/scripts/choices.min.js'>
-    </script>
-    <script src="../theme/admin/html/assets/libs/%40popperjs/core/umd/popper.min.js"></script>
-    <script src="../theme/admin/html/assets/libs/tippy.js/tippy-bundle.umd.min.js"></script>
-    <script src="../theme/admin/html/assets/libs/simplebar/simplebar.min.js"></script>
-    <script src="../theme/admin/html/assets/libs/prismjs/prism.js"></script>
-    <script src="../theme/admin/html/assets/libs/lucide/umd/lucide.js"></script>
-    <script src="../theme/admin/html/assets/js/starcode.bundle.js"></script>
-    <script src="../theme/admin/html/assets/js/pages/auth-login.init.js"></script>
+    <script src='{{ asset('admin/libs/choices.js/public/admin/scripts/choices.min.js') }}'></script>
+    <script src="{{ asset('admin/libs/%40popperjs/core/umd/popper.min.js') }}"></script>
+    <script src="{{ asset('admin/libs/tippy.js/tippy-bundle.umd.min.js') }}"></script>
+    <script src="{{ asset('admin/libs/simplebar/simplebar.min.js') }}"></script>
+    <script src="{{ asset('admin/libs/prismjs/prism.js') }}"></script>
+    <script src="{{ asset('admin/libs/lucide/umd/lucide.js') }}"></script>
+    <script src="{{ asset('admin/js/starcode.bundle.js') }}"></script>
+    <script src="{{ asset('admin/js/pages/auth-login.init.js') }}"></script>
 
 </body>
 
