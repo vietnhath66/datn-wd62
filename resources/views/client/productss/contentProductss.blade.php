f9a0832517070ee4d456e3570107e9280898c5ea
phunghieu-2004 <phieu9452@gmail.com>
2025-04-25 00:51:25 +0700

    filter'

--- resources/views/client/productss/contentProductss.blade.php
+++ resources/views/client/productss/contentProductss.blade.php
@@ -40,13 +40,19 @@
                 <div class="flex-c-m stext-106 cl6 size-104 bor4 pointer hov-btn3 trans-04 m-r-8 m-tb-4 js-show-filter">
                     <i class="icon-filter cl2 m-r-6 fs-15 trans-04 zmdi zmdi-filter-list"></i>
                     <i class="icon-close-filter cl2 m-r-6 fs-15 trans-04 zmdi zmdi-close dis-none"></i>
-                    Filter
+                    Lọc
                 </div>
 
-                <div class="flex-c-m stext-106 cl6 size-105 bor4 pointer hov-btn3 trans-04 m-tb-4 js-show-search">
-                    <i class="icon-search cl2 m-r-6 fs-15 trans-04 zmdi zmdi-search"></i>
-                    <i class="icon-close-search cl2 m-r-6 fs-15 trans-04 zmdi zmdi-close dis-none"></i>
-                    Search
+                <div class="flex-w flex-c-m m-tb-10">
+                    <div class="flex-c-m stext-106 cl6 size-104 bor4 pointer  trans-04 m-r-8 m-tb-4 ">
+                        <form action="{{ route('client.viewSearch') }}" method="GET" class="flex-w p-l-15">
+                            <button type="submit" class="flex-c-m trans-04">
+                                <i class="zmdi zmdi-search" style="padding-right: 5px"></i>
+                            </button>
+                            <input class="plh3 kanit-thin" type="text" name="search" placeholder="Tìm kiếm"
+                                required />
+                        </form>
+                    </div>
                 </div>
             </div>
 
@@ -58,200 +64,63 @@
                     </button>
 
                     <input class="mtext-107 cl2 size-114 plh2 p-r-15" type="text" name="search-product"
-                        placeholder="Search">
+                        placeholder="Search" />
                 </div>
             </div>
             <!-- Filter -->
             <div class="dis-none panel-filter w-full p-t-10">
                 <div class="wrap-filter flex-w bg6 w-full p-lr-40 p-t-27 p-lr-15-sm">
                     <div class="filter-col1 p-r-15 p-b-27">
-                        <div class="mtext-102 cl2 p-b-15">
-                            Sort By
-                        </div>
-
+                        <div class="mtext-102 cl2 p-b-15">Sắp xếp theo</div>
                         <ul>
                             <li class="p-b-6">
-                                <a href="#" class="filter-link stext-106 trans-04">
-                                    Default
-                                </a>
+                                <button class="filter-btn stext-106 trans-04" data-sort="name_asc">
+                                    Thứ tự bảng chữ cái (từ A-Z)
+                                </button>
                             </li>
-
                             <li class="p-b-6">
-                                <a href="#" class="filter-link stext-106 trans-04">
-                                    Popularity
-                                </a>
-                            </li>
-
-                            <li class="p-b-6">
-                                <a href="#" class="filter-link stext-106 trans-04">
-                                    Average rating
-                                </a>
-                            </li>
-
-                            <li class="p-b-6">
-                                <a href="#" class="filter-link stext-106 trans-04 filter-link-active">
-                                    Newness
-                                </a>
-                            </li>
-
-                            <li class="p-b-6">
-                                <a href="#" class="filter-link stext-106 trans-04">
-                                    Price: Low to High
-                                </a>
-                            </li>
-
-                            <li class="p-b-6">
-                                <a href="#" class="filter-link stext-106 trans-04">
-                                    Price: High to Low
-                                </a>
+                                <button class="filter-btn stext-106 trans-04" data-sort="name_desc">
+                                    Thứ tự bảng chữ cái (từ Z-A)
+                                </button>
                             </li>
                         </ul>
                     </div>
-
                     <div class="filter-col2 p-r-15 p-b-27">
-                        <div class="mtext-102 cl2 p-b-15">
-                            Price
-                        </div>
-
+                        <div class="mtext-102 cl2 p-b-15">Giá</div>
                         <ul>
                             <li class="p-b-6">
-                                <a href="#" class="filter-link stext-106 trans-04 filter-link-active">
-                                    All
-                                </a>
+                                <button class="filter-btn stext-106 trans-04" data-sort="price_asc">
+                                    Giá (từ thấp đến cao)
+                                </button>
                             </li>
-
                             <li class="p-b-6">
-                                <a href="#" class="filter-link stext-106 trans-04">
-                                    $0.00 - $50.00
-                                </a>
-                            </li>
-
-                            <li class="p-b-6">
-                                <a href="#" class="filter-link stext-106 trans-04">
-                                    $50.00 - $100.00
-                                </a>
-                            </li>
-
-                            <li class="p-b-6">
-                                <a href="#" class="filter-link stext-106 trans-04">
-                                    $100.00 - $150.00
-                                </a>
-                            </li>
-
-                            <li class="p-b-6">
-                                <a href="#" class="filter-link stext-106 trans-04">
-                                    $150.00 - $200.00
-                                </a>
-                            </li>
-
-                            <li class="p-b-6">
-                                <a href="#" class="filter-link stext-106 trans-04">
-                                    $200.00+
-                                </a>
+                                <button class="filter-btn stext-106 trans-04" data-sort="price_desc">
+                                    Giá (từ cao đến thấp)
+                                </button>
                             </li>
                         </ul>
                     </div>
-
-                    <div class="filter-col3 p-r-15 p-b-27">
-                        <div class="mtext-102 cl2 p-b-15">
-                            Color
-                        </div>
-
-                        <ul>
-                            <li class="p-b-6">
-                                <span class="fs-15 lh-12 m-r-6" style="color: #222;">
-                                    <i class="zmdi zmdi-circle"></i>
-                                </span>
-
-                                <a href="#" class="filter-link stext-106 trans-04">
-                                    Black
-                                </a>
-                            </li>
-
-                            <li class="p-b-6">
-                                <span class="fs-15 lh-12 m-r-6" style="color: #4272d7;">
-                                    <i class="zmdi zmdi-circle"></i>
-                                </span>
-
-                                <a href="#" class="filter-link stext-106 trans-04 filter-link-active">
-                                    Blue
-                                </a>
-                            </li>
-
-                            <li class="p-b-6">
-                                <span class="fs-15 lh-12 m-r-6" style="color: #b3b3b3;">
-                                    <i class="zmdi zmdi-circle"></i>
-                                </span>
-
-                                <a href="#" class="filter-link stext-106 trans-04">
-                                    Grey
-                                </a>
-                            </li>
-
-                            <li class="p-b-6">
-                                <span class="fs-15 lh-12 m-r-6" style="color: #00ad5f;">
-                                    <i class="zmdi zmdi-circle"></i>
-                                </span>
-
-                                <a href="#" class="filter-link stext-106 trans-04">
-                                    Green
-                                </a>
-                            </li>
-
-                            <li class="p-b-6">
-                                <span class="fs-15 lh-12 m-r-6" style="color: #fa4251;">
-                                    <i class="zmdi zmdi-circle"></i>
-                                </span>
-
-                                <a href="#" class="filter-link stext-106 trans-04">
-                                    Red
-                                </a>
-                            </li>
-
-                            <li class="p-b-6">
-                                <span class="fs-15 lh-12 m-r-6" style="color: #aaa;">
-                                    <i class="zmdi zmdi-circle-o"></i>
-                                </span>
-
-                                <a href="#" class="filter-link stext-106 trans-04">
-                                    White
-                                </a>
-                            </li>
-                        </ul>
+                    <div class="filter-col1 p-r-15 p-b-27">
+                        <div class="mtext-102 cl2 p-b-15">Size</div>
+                        @foreach ($colors as $color)
+                            <label style="display: inline;">
+                                <input type="checkbox" name="sizes[]" value="{{ Str::slug($color->name) }}">
+                                {{ $color->name }}
+                            </label><br>
+                        @endforeach
                     </div>
 
-                    <div class="filter-col4 p-b-27">
-                        <div class="mtext-102 cl2 p-b-15">
-                            Tags
-                        </div>
+                    <div>
+                        <div class="mtext-102 cl2 p-b-15">Màu sắc</div>
+                        @foreach ($sizes as $size)
+                            <label style="display: inline;">
+                                <input type="checkbox" name="colors[]" value="{{ Str::slug($size->name) }}">
+                                {{ $size->name }}
+                            </label><br>
+                        @endforeach
 
-                        <div class="flex-w p-t-4 m-r--5">
-                            <a href="#"
-                                class="flex-c-m stext-107 cl6 size-301 bor7 p-lr-15 hov-tag1 trans-04 m-r-5 m-b-5">
-                                Fashion
-                            </a>
-
-                            <a href="#"
-                                class="flex-c-m stext-107 cl6 size-301 bor7 p-lr-15 hov-tag1 trans-04 m-r-5 m-b-5">
-                                Lifestyle
-                            </a>
-
-                            <a href="#"
-                                class="flex-c-m stext-107 cl6 size-301 bor7 p-lr-15 hov-tag1 trans-04 m-r-5 m-b-5">
-                                Denim
-                            </a>
-
-                            <a href="#"
-                                class="flex-c-m stext-107 cl6 size-301 bor7 p-lr-15 hov-tag1 trans-04 m-r-5 m-b-5">
-                                Streetstyle
-                            </a>
-
-                            <a href="#"
-                                class="flex-c-m stext-107 cl6 size-301 bor7 p-lr-15 hov-tag1 trans-04 m-r-5 m-b-5">
-                                Crafts
-                            </a>
-                        </div>
                     </div>
+
                 </div>
             </div>
         </div>
@@ -260,7 +129,19 @@
 
         <div class="row isotope-grid">
             @foreach ($products as $product)
-                <div class="col-sm-6 col-md-4 col-lg-3 p-b-35 isotope-item women">
+            @php
+                    $colorClasses = '';
+                    $sizeClasses = '';
+                    foreach ($product->variants as $variant) {
+                        if ($variant->name_variant_color) {
+                            $colorClasses .= ' color-' . Str::slug($variant->name_variant_color);
+                        }
+                        if ($variant->name_variant_size) {
+                            $sizeClasses .= ' size-' . Str::slug($variant->name_variant_size);
+                        }
+                    }
+                @endphp
+                <div class="col-sm-6 col-md-4 col-lg-3 p-b-35 isotope-item women {{ $colorClasses }} {{ $sizeClasses }}">
                     <!-- Block2 -->
                     <div class="block2">
                         <div class="block2-pic hov-img0">
@@ -286,10 +167,10 @@
                                 </style>
 
                                 <a href="{{ Route('client.product.show', $product->id) }}"
-                                    class="stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6 black-bold-link">
+                                    class="stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6 black-bold-link product-name">
                                     {{ $product->name }}
                                 </a>
-                                <span class="stext-105 cl3">
+                                <span class="stext-105 cl3 product-price">
                                     {{ number_format($product->price, 0, ',', '.') }} VNĐ
                                 </span>
                             </div>
@@ -307,6 +188,9 @@
                 </div>
             @endforeach
 
+        </div>
+        <div id="no-products" style="display: none;">
+            <p>Không có sản phẩm nào trong mục này.</p>
         </div>
 
         <!-- Load more -->
