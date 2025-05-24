4c44e58feeb7041c1538190db4a9518608fc405a
lehoang200404 <le920075@gmail.com>
2025-05-24 15:41:44 +0700

    update locked user

--- app/Models/User.php
+++ app/Models/User.php
@@ -55,6 +55,7 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
+        'is_locked' => 'boolean',
     ];
 
     public function roles()
